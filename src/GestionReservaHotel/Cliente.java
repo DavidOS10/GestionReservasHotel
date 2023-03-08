b592e82dcf3270e9768367198a4c608e4252ce11
DavidOS10 <onatesanchezdavid@gmail.com>
2023-03-08 13:51:31 +0100

    Clases cliente y reserva de David

--- src/GestionReservaHotel/Cliente.java
+++ src/GestionReservaHotel/Cliente.java
@@ -5,8 +5,70 @@ package GestionReservaHotel;
 
 /**
  * @author David.O
- *
+ * Generamos la clase cliente,
  */
 public class Cliente {
 
+	/**
+	 * Creamos los atributos de la clase 
+	 */
+	
+	private int numeroCliente;
+	private String nombreCliente;
+	private int telefono;
+	
+	/**
+	 * Generamos getter de numeroCliente.
+	 * @return
+	 */
+	
+	public int getNumeroCliente() {
+		return numeroCliente;
+	}
+	
+	/**
+	 * Generamos setter de fechaSalida.
+	 * @param numeroReserva
+	 */
+	
+	public void setNumeroCliente(int numeroCliente) {
+		this.numeroCliente = numeroCliente;
+	}
+	
+	/**
+	 * Generamos getter de nombreCliente.
+	 * @return
+	 */
+	
+	public String getNombreCliente() {
+		return nombreCliente;
+	}
+	
+	/**
+	 * Generamos setter de fechaSalida.
+	 * @param numeroReserva
+	 */
+	
+	public void setNombreCliente(String nombreCliente) {
+		this.nombreCliente = nombreCliente;
+	}
+	
+	/**
+	 * Generamos getter de telefono.
+	 * @return
+	 */
+	
+	public int getTelefono() {
+		return telefono;
+	}
+	
+	/**
+	 * Generamos setter de fechaSalida.
+	 * @param numeroReserva
+	 */
+	
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	
 }
--- src/GestionReservaHotel/Reserva.java
+++ src/GestionReservaHotel/Reserva.java
@@ -3,10 +3,104 @@
  */
 package GestionReservaHotel;
 
+import java.util.Date;
+
 /**
- * @author david
- *
+ * @author David.O
+ * Clase Reserva
  */
 public class Reserva {
 
+	/**
+	 * Introducimos las variables
+	 */
+	
+	private int numeroReserva;
+	private Date fechaEntrada;
+	private Date fechaSalida;
+	private int numPersonas;
+
+	/**
+	 * Generamos getter de numeroReserva.
+	 * @return
+	 */
+	public int getNumeroReserva() {
+		return numeroReserva;
+	}
+
+	/**
+	 * Generamos setter de numeroReserva.
+	 * @param numeroReserva
+	 */
+	public void setNumeroReserva(int numeroReserva) {
+		this.numeroReserva = numeroReserva;
+	}
+
+	/**
+	 * Generamos getter de fechaEntrada.
+	 * @return
+	 */
+	
+	public Date getFechaEntrada() {
+		return fechaEntrada;
+	}
+
+	/**
+	 * Generamos setter de fechaEntrada.
+	 * @param numeroReserva
+	 */
+	
+	public void setFechaEntrada(Date fechaEntrada) {
+		this.fechaEntrada = fechaEntrada;
+	}
+
+	/**
+	 * Generamos getter de fechaSalida.
+	 * @return
+	 */
+	
+	public Date getFechaSalida() {
+		return fechaSalida;
+	}
+
+	/**
+	 * Generamos setter de fechaSalida.
+	 * @param numeroReserva
+	 */
+	
+	public void setFechaSalida(Date fechaSalida) {
+		this.fechaSalida = fechaSalida;
+	}
+
+	/**
+	 * Generamos getter de numPersonas.
+	 * @return
+	 */
+	
+	public int getNumPersonas() {
+		return numPersonas;
+	}
+	
+	/**
+	 * Generamos setter de numPersonas.
+	 * @param numeroReserva
+	 */
+
+	public void setNumPersonas(int numPersonas) {
+		this.numPersonas = numPersonas;
+	}
+	
+	/**
+	 * Generamos metodo calcularPrecio
+	 */
+    public void calcularPrecio() {
+		
+	}
+	
+    /**
+     * Generamos el metodo modificar reserva.
+     */
+	public void modificarReserva() {
+		
+	}
 }
